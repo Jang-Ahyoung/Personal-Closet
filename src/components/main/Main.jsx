3bc05443a975f867083c220578ab3be80b4bf46f
Jang-Ahyoung <wegotthelove27@gmail.com>
2021-08-06 21:44:47 +0900

    📝 Main 컴포넌트 생성

---
+++ src/components/main/Main.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styles from "./Main.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSortUp } from "@fortawesome/free-solid-svg-icons";
+import { Link, useHistory } from "react-router-dom";
+
+
+function Main() {
+    const history = useHistory();
+    return (
+        <div className={styles.container}>
+            <div className={styles.logo}>My Closet</div>
+            <img className={styles.imgLeft} alt="CodiImg" src="img/1.jpg" onClick={() => history.push({ pathname: '/home', state: { type: "closet" } })}></img>
+            <div className={styles.textLeft} >
+                <FontAwesomeIcon icon={faSortUp} /><span className={styles.text}> My clothes</span>
+            </div>
+            <img className={styles.imgRight} alt="CodiImg" src="img/main_gray.jpg" onClick={() => history.push({ pathname: '/home', state: { type: "codi" } })}></img>
+            <div className={styles.textRight} >
+                <FontAwesomeIcon icon={faSortUp} /> <span className={styles.text}> My codi</span>
+            </div>
+            <img className={styles.img} alt="CodiImg" src="img/acc.jpg"></img>
+        </div>
+    );
+}
+
+export default Main;
