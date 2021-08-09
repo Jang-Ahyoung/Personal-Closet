3c9ba03b9bbd2ad3ea4630a613cdec953bd4b4d9
Jang-Ahyoung <wegotthelove27@gmail.com>
2021-08-09 23:12:00 +0900

    ✍의류 정보 UPDATE 컴포넌트 수정

--- src/components/clothesUpdate/ClothesUpdate.jsx
+++ src/components/clothesUpdate/ClothesUpdate.jsx
@@ -1,8 +1,7 @@
 import React, { useRef, useState } from 'react';
 import styles from './ClothesUpdate.module.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUpload } from "@fortawesome/free-solid-svg-icons";
-import { useHistory } from 'react-router-dom';
+import { faUpload, faHeart, faShoppingCart, faUserAstronaut, faStore, faTshirt, faUserTag } from "@fortawesome/free-solid-svg-icons";
 import BackArrowHeader from '../backArrowHeader/BackArrowHeader';
 
 
@@ -11,12 +10,15 @@ function ClothesUpdate() {
     const [category, setCategory] = useState(null);
     const [price, setPrice] = useState(null);
     const [color, setColor] = useState(null);
+    const [fabric, setFabric] = useState(null);
     const [size, setSize] = useState(null);
+    const [brand, setBrand] = useState(null);
+    const [date, setDate] = useState(null);
+    const [photo, setPhoto] = useState(null);
+
     const inputRef = useRef();
-    const history = useHistory();
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(name, category, price, color, size);
     }
     const onButtonClick = event => {
         event.preventDefault();
@@ -33,7 +35,12 @@ function ClothesUpdate() {
                     <BackArrowHeader />
                     <p className={styles.headerTitle}> + New Clothes</p>
                     <p className={styles.headerDesc}> 상품명 & 카테고리 <br /> 구매 가격, 사이즈, 색상을 입력해주세요.</p>
-                    <input className={styles.title} type="text" placeholder="상품명" onChange={(e) => setName(e.target.value)} required></input>
+                    <div className={styles.title}>
+                        <FontAwesomeIcon icon={faUserAstronaut} className={`${styles.inputIcon} ${styles.nameIcon}`} />
+                        <div className={styles.inputSection}>
+                            <input className={styles.titleContext} type="text" placeholder="상품명" onChange={(e) => setName(e.target.value)} required />
+                        </div>
+                    </div>
                     <section className={styles.middle}>
                         <div className={styles.category}>
                             <select onChange={(e) => setCategory(e.target.value)} required>
@@ -45,20 +52,52 @@ function ClothesUpdate() {
                                 <option>Acc</option>
                             </select>
                         </div>
-                        <div >
-                            \ <input type="number" className={styles.price} placeholder="가격" onChange={(e) => setPrice(e.target.value)}></input>
+                        <div className={styles.price}>
+                            <FontAwesomeIcon icon={faHeart} className={`${styles.priceIcon} ${styles.nameIcon}`} />
+                            <input className={styles.priceContext} type="number" placeholder="가격" onChange={(e) => setPrice(e.target.value)}></input>
                         </div>
                     </section>
                 </div>
                 <div className={styles.bottom}>
                     <div className={styles.ImageInput} >
-                        <input className={styles.Prodectinput} ref={inputRef} onChange={onChange} type="file" accept="image/*" name="file" onChange={(e) => setCategory(e.target.value)}></input>
+                        <input className={styles.Prodectinput} ref={inputRef} onChange={onChange} type="file" accept="image/*" name="file" onChange={(e) => setPhoto(e.target.value)}></input>
                         <FontAwesomeIcon className={styles.ImageIcon} icon={faUpload} onClick={onButtonClick} />
                     </div>
                     <div className={styles.ProductDesc}>
-                        <section className={styles.section}>
-                            <input className={styles.inputSize} type="text" placeholder="Size" onChange={(e) => setSize(e.target.value)} />
-                        </section>
+                        <div className={styles.rightSide}>
+                            <div className={styles.inputLine}>
+                                <div className={styles.inputContainer}>
+                                    <FontAwesomeIcon icon={faTshirt} className={`${styles.inputIcon} ${styles.tshirtIcon}`} />
+                                    <div className={styles.inputSection}>
+                                        <p className={styles.inputTitle}>소재</p>
+                                        <input className={styles.inputContext} type="text" placeholder="소재를 입력해주세요 . . ." onChange={(e) => setFabric(e.target.value)} />
+                                    </div>
+                                </div>
+                                <div className={styles.inputContainer}>
+                                    <FontAwesomeIcon icon={faUserTag} className={`${styles.inputIcon} ${styles.sizeIcon}`} />
+                                    <div className={styles.inputSection}>
+                                        <p className={styles.inputTitle}>Size</p>
+                                        <input className={styles.inputContext} type="text" placeholder="사이즈를 입력해주세요 . . ." onChange={(e) => setSize(e.target.value)} />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={styles.inputLine}>
+                                <div className={styles.inputContainer}>
+                                    <FontAwesomeIcon icon={faStore} className={`${styles.inputIcon} ${styles.storeIcon}`} />
+                                    <div className={styles.inputSection}>
+                                        <p className={styles.inputTitle}>Brand</p>
+                                        <input className={styles.inputContext} type="text" placeholder="브랜드를 입력해주세요 . . ." onChange={(e) => setBrand(e.target.value)} />
+                                    </div>
+                                </div>
+                                <div className={styles.inputContainer}>
+                                    <FontAwesomeIcon icon={faShoppingCart} className={`${styles.inputIcon} ${styles.cartIcon}`} />
+                                    <div className={styles.inputSection}>
+                                        <p className={styles.inputTitle}>구매 일자</p>
+                                        <input className={styles.inputContext} type="date" placeholder="구매 날짜 입력 . . ." onChange={(e) => setDate(e.target.value)} />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                         <section className={styles.colorContainer}>
                             <p className={styles.description}>Main Color</p>
                             <input className={styles.colorinput} type="color" placeholder="색상" onChange={(e) => setColor(e.target.value)}></input>
