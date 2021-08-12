55481c2cbdb5a9cbbab6344f17e8eb6b421998ac
Jang-Ahyoung <wegotthelove27@gmail.com>
2021-08-12 22:22:37 +0900

    🔍 의류 선택 컴포넌트 수정

--- src/components/clothesSelect/ClothesSelect.jsx
+++ src/components/clothesSelect/ClothesSelect.jsx
@@ -11,44 +11,51 @@ function ClothesSelect() {
         setSearchText(e.target.value);
     }
     let clothesData = searchText ? CLOTHES_DATA.filter((data) => data.name.toLowerCase().includes(searchText.toLowerCase())) : CLOTHES_DATA;
+    const [smallCategory, setSmallCategory] = useState(0);
+    const smallCategoryArray = ['전체', '나시/반팔', '티셔츠/맨투맨', '블라우스/셔츠', '니트', '스포츠웨어'];
 
     return (
         <div className={styles.conatiner}>
             <div className={styles.closeIcon} >
                 <span>Top</span>
                 <FontAwesomeIcon icon={faTimesCircle} className={styles.Icon} /></div>
-            <SearchBar handleSearch={handleSearch} />
-            <div className={styles.productContainer}>
-                {
-                    clothesData.map((data) =>
-                        <div className={styles.product}>
-                            <img className={styles.productImg} src="img/cloth/dress.jpg"></img>
-                            <div className={styles.productDesc}>
-                                <div className={styles.productName}>{data.name}</div>
-                                <section className={styles.productDetail}>
-                                    <div className={styles.productSection}>
-                                        <p className={styles.productLabel}>price</p>
-                                        <p className={styles.productInfo}>\ 20000</p>
+
+            <div className={styles.section}>
+                <SearchBar handleSearch={handleSearch} />
+                <div className={styles.miniCategory}>
+                    {
+                        smallCategoryArray.map((categoty, index) => {
+                            return <button className={`${styles.miniCategory_text} ${smallCategory == index && styles.seleted}`} onClick={() => setSmallCategory(index)}>{categoty}</button>
+                        })
+                    }
+                </div>
+                <div className={styles.productContainer}>
+                    {
+                        clothesData.map((data) =>
+                            <div className={styles.product}>
+                                <div className={styles.imgContainer}>
+                                    <img className={styles.productImg} src="img/cloth/dress.jpg"></img>
+                                </div>
+                                <div className={styles.productDesc}>
+                                    <div>
+                                        <p className={styles.productdate}><span className={styles.dateText}>마지막 착용 _ </span>4 days ago</p>
+                                        <div className={styles.productName}>{data.name}</div>
                                     </div>
-                                    <div className={styles.productSection}>
-                                        <p className={styles.productLabel}>구매일</p>
-                                        <p className={styles.productInfo}>2021.10.32</p>
-                                    </div>
-                                    <div className={styles.productSection}>
-                                        <p className={styles.productLabel}>Brand</p>
-                                        <p className={styles.productInfo}>{data.brandName || '-'}</p>
-                                    </div>
-                                    <div className={styles.productSection}>
-                                        <p className={styles.productLabel}>선호도</p>
-                                        <p className={styles.productInfo}>Good</p>
-                                    </div>
-                                </section>
+                                    <section className={styles.productDetail}>
+                                        <p className={styles.productBrand}>Adidas</p>
+                                        <p className={styles.productWearNum}>착용 횟수 : 4번</p>
+                                    </section>
+                                </div>
                             </div>
-                        </div>
-                    )
-                }
+                        )
+                    }
+                </div>
             </div>
-        </div>
+            <div className={styles.bottom}>
+                <button className={styles.addBtn}>+ Add New</button>
+            </div>
+
+        </div >
     )
 }
 
