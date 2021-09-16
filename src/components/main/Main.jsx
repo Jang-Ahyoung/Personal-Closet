f0891b9fd4d86c9a78f9637427816dc30bb5b7e9
Jang-Ahyoung <wegotthelove27@gmail.com>
2021-09-16 20:54:15 +0900

    Main 컴포넌트 코드 및 모바일 화면 조절

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,25 +1,30 @@
 import React from "react";
 import styles from "./Main.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSortUp } from "@fortawesome/free-solid-svg-icons";
-import { Link, useHistory } from "react-router-dom";
+import { faCaretRight, faSortUp } from "@fortawesome/free-solid-svg-icons";
+import { useHistory } from "react-router-dom";
 
 
 function Main() {
     const history = useHistory();
+    const handleHistory = (type) => {
+        history.push({ pathname: "/collection", state: { type: type } })
+    }
     return (
         <div className={styles.container}>
             <div className={styles.logo}>My Closet</div>
-            <img className={styles.imgLeft} alt="CodiImg" src="img/cloth/clothing-store.jpg" onClick={() => history.push({ pathname: '/home', state: { type: "closet" } })}></img>
+
+            <img className={styles.imgLeft} alt="CodiImg" src="img/cloth/clothing-store.jpg" onClick={() => handleHistory('closet')}></img>
             <div className={styles.textLeft} >
-                <FontAwesomeIcon icon={faSortUp} /><span className={styles.text}> My clothes</span>
+                <FontAwesomeIcon icon={faSortUp} />
+                <span className={styles.text} onClick={() => handleHistory('closet')}> My clothes</span>
             </div>
-            <img className={styles.imgRight} alt="CodiImg" src="img/ocean.jpg" onClick={() => history.push({ pathname: '/home', state: { type: "codi" } })}></img>
+
+            <img className={styles.imgRight} alt="CodiImg" src="img/ocean.jpg" onClick={() => handleHistory('codi')}></img>
             <div className={styles.textRight} >
-                <FontAwesomeIcon icon={faSortUp} /> <span className={styles.text}> My codi</span>
+                <FontAwesomeIcon icon={faCaretRight} /> <span className={styles.textCodi} onClick={() => handleHistory('codi')}> My codi</span>
             </div>
-            <img className={styles.img} alt="CodiImg" src="img/acc.jpg"></img>
-        </div>
+        </div >
     );
 }
 
